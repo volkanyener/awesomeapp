2206d917cabda432a321865e9a9dbb3e65ce6f9a
volkan <volkan>
2020-06-30 14:40:37 +0200

    koffie maken

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>React is Cool!</p>
-      <p>testing 123</p>
+      <p>Selinoskommm!!!</p>
+      <h1>Seni coooook seviyorum!!!</h1>
+      <h1>Kun je misschien een koffietje voor mij maken??</h1>
+      <h1>LOVE YOUUU!</h1>
     </div>
   );
 }
