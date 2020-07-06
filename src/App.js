11f2382fea1159e993e1a50cdfa22ba9305ed28e
volkan <volkan>
2020-07-06 16:55:32 +0200

    added a title component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Title from './components/Title';
 
 
 function App() {
   return (
     <div className="App">
-      <p>Selinoskommm!!!</p>
-      <h1>Seni coooook seviyorum!!!</h1>
-      <h1>Kun je misschien een koffietje voor mij maken??</h1>
-      <h1>LOVE YOUUU!</h1>
+      <main>
+        <Title />
+      </main>
+
+      <header>
+        <p>Selinoskommm!!!</p>
+        <h1>Seni coooook seviyorum!!!</h1>
+        <h1>Kun je misschien een koffietje voor mij maken??</h1>
+        <h1>LOVE YOUUU!</h1>
+      </header>
+        
     </div>
   );
 }
