7c431ef95e800276e3ef7abd16d2fdf88ff1b024
volkan <volkan>
2020-07-09 14:39:47 +0200

    finished awesomeapp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import Title from './components/Title';
+import Pokemon from './components/Pokemon/Pokemon';
 
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"]
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Some Simple Title" />
+      <div className="col-md-6 col-lg-4">
+        {all_pokemon.map(pokemon => {
+          return (
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          )
+        })}    
+        </div>      
       </main>
-
-      <header>
-        <p>Selinoskommm!!!</p>
-        <h1>Seni coooook seviyorum!!!</h1>
-        <h1>Kun je misschien een koffietje voor mij maken??</h1>
-        <h1>LOVE YOUUU!</h1>
-      </header>
-        
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Title() {
-    return <h1>Hi there !</h1>
+export default function Title(props) {
+    return <h1>{props.content}</h1>
 }
